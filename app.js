8e23d3736519809897cb29932cf0503252564c48
pmatsuda <pat.matsuda@comcast.net>
2019-10-17 20:37:04 -0500

    Import Mongoose, connect to database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +14,9 @@ var config = require('./config.dev');
 
 //Test the file
 console.log(config);
+
+//Connect to MongoDB
+mongoose.connect(config.mongodb, { useNewUrlParser: true });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
