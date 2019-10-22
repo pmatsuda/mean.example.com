59d822a2ba111a92056b7cf7eb631f2b1647a739
pmatsuda <pat.matsuda@comcast.net>
2019-10-21 22:31:49 -0500

    Add authentication fields to users model

--- models/users.js
+++ models/users.js
@@ -27,7 +27,21 @@ var Users = new Schema({
   modified: {
     type: Date,
     default: Date.now
-  }
+  },
+  hash: {
+    type: String,
+    required: [
+      true,   
+      'There was a problem creating your password'
+    ]
+  },
+  salt: {
+    type: String,
+    required: [
+      true, 
+      'There was a problem creating your password'
+    ]
+  },
 });
 
 Users.pre('save', function(next){
