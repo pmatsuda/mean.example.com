f2873924deda028774efd034625a4bf600244751
pmatsuda <pat.matsuda@comcast.net>
2019-10-21 23:52:35 -0500

    Add non-API endpoint for accessing the auth app

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiUsersRouter = require('./routes/api/users');
 var apiAuthRouter = require('./routes/api/auth');
+var authRouter = require('./routes/auth');
 
 var app = express();
 
@@ -72,6 +73,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/users', apiUsersRouter);
 app.use('/api/auth', apiAuthRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
