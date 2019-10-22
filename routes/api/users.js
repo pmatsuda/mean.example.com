468ea459e598ec834d726d7ad09a48b88e05e060
pmatsuda <pat.matsuda@comcast.net>
2019-10-21 22:17:24 -0500

    add update and delete record, install passport

--- routes/api/users.js
+++ routes/api/users.js
@@ -28,4 +28,62 @@ router.post('/', function(req, res) {
   });
 });
 
+router.put('/', function(req, res){
+
+  Users.findOne({'_id': req.body._id}, function(err, user){
+
+   if(err) {
+     return res.json({success: false, error: err});
+   }
+
+   if(user) {
+
+    let data = req.body;
+
+    if(data.username){
+      user.username = data.username;
+    };
+
+    if(data.email){
+    user.email = data.email;
+    };
+
+    if(data.first_name){
+    user.first_name = data.first_name;
+    };
+
+    if(data.last_name){
+    user.last_name = data.last_name;
+    };
+
+    user.save(function(err){
+      if(err){
+        return res.json({success: false, error: err});
+      }else{
+        return res.json({success: true, user:user});
+      }
+    });
+
+   }
+
+  });
+  
+});
+
+router.delete('/:userId', function(req,res){
+
+  var userId = req.params.userId;
+
+  Users.remove({'_id':userId}, function(err,removed){
+
+    if(err){
+      return res.json({success: false, error: err});
+    }
+
+    return res.json({success: true, status: removed});
+
+  });
+
+});
+
 module.exports = router;
