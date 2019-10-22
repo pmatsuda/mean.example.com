19404de1810017c8c1fed0c07c761661b3010428
pmatsuda <pat.matsuda@comcast.net>
2019-10-21 19:50:46 -0500

    Create a new user record

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,4 +11,21 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/', function(req, res) {
+  Users.create(new Users({
+    username: req.body.username,
+    email: req.body.email,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name
+  }), function(err, user){
+    
+    if(err){
+      return res.json({success: false, user: req.body, error: err});
+    }
+
+    return res.json({success: true, user: user});
+    
+  });
+});
+
 module.exports = router;
