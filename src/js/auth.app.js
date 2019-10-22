76ce4de0ffc75ada40873166f89356efbea42cf8
pmatsuda <pat.matsuda@comcast.net>
2019-10-22 00:03:30 -0500

    Add error/success handling to the logi form

--- src/js/auth.app.js
+++ src/js/auth.app.js
@@ -26,9 +26,38 @@ var authApp = (function() {
       app.innerHTML=form;
     }
   
+    function postRequest(formId, url){
+        let form = document.getElementById(formId);
+        form.addEventListener('submit', function(e){
+          e.preventDefault();
+      
+          let formData = new FormData(form);
+          let uri = `${window.location.origin}${url}`;
+          let xhr = new XMLHttpRequest();
+          xhr.open('POST', uri);
+      
+          xhr.setRequestHeader(
+            'Content-Type',
+            'application/json; charset=UTF-8'
+          );
+      
+          let object = {};
+          formData.forEach(function(value, key){
+            object[key]=value;
+          });
+      
+          xhr.send(JSON.stringify(object));
+          xhr.onload = function(){
+            let data = JSON.parse(xhr.response);
+            console.log(data);
+          }
+        });
+      }
+ 
     return {
       load: function(){
         loginForm();
+        postRequest('loginForm', '/api/auth/login');
       }
     }
   
