0a7e157665728c7b836d1466d4c6f0075ad19275
pmatsuda <pat.matsuda@comcast.net>
2019-10-21 22:23:57 -0500

    configure session and cookie data

--- config.dev.js
+++ config.dev.js
@@ -1,3 +1,17 @@
 var config = {};
 config.mongodb = 'mongodb://localhost/mean-cms';
 module.exports = config;
+
+//Session configuration object
+config.session = {};
+
+//Cookie configuration object
+config.cookie = {};
+
+//Create a random string to sign the session data
+//Bigger is better, more entropy is better
+//The is OK for dev, change for production
+config.session.secret = '7j&1tH!cr4F*1U';
+
+//Define the domain for which this cookie is to be set
+config.cookie.domain = 'localhost';
