32f50539465284c12e2db5e22b067cd09bad98a9
pmatsuda <pat.matsuda@comcast.net>
2019-10-22 20:09:50 -0500

    Display user in HTML table

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -1,30 +1,77 @@
-var usersApp = (function() {
+var usersApp = (function () {
 
-  function viewUsers(){
+  function viewUsers() {
 
     let uri = `${window.location.origin}/api/users`;
     let xhr = new XMLHttpRequest();
     xhr.open('GET', uri);
-  
+
     xhr.setRequestHeader(
       'Content-Type',
       'application/json; charset=UTF-8'
     );
-  
+
     xhr.send();
-  
-    xhr.onload = function(){
+
+    xhr.onload = function () {
+      let app = document.getElementById('app');
       let data = JSON.parse(xhr.response);
-      console.log(data);
+      let users = data.users;
+      let table = '';
+      let rows = '';
+
+      //Loop each user record into it's own HTML table row, each user should
+      //have a link a user view
+      for (let i = 0; i < users.length; i++) {
+        rows = rows + `<tr>
+          <td>
+            <a href="#view-${users[i]['_id']}">${users[i]['last_name']}, ${users[i]['first_name']}</a>
+          </td>
+          <td>${users[i]['username']}</td>
+          <td>${users[i]['email']}</td>
+        </tr>`;
+      }
+
+      //Create a users panel, add a table to the panel, inject the rows into the
+      //table
+      table = `<div class="card">
+        <div class="card-header clearfix">
+          <h2 class="h3 float-left">Users</h2>
+          <div class="float-right">
+            <a href="#create" class="btn btn-primary">New User</a>
+          </div>
+        </div>
+        <div class="table-responsive">
+          <table class="table table-striped table-hover table-bordered">
+            <thead>
+              <tr>
+                <td>Name</td>
+                <td>Username</td>
+                <td>Email</td>
+              </tr>
+            </thead>
+            <tbody>${rows}</tbody>
+          </table>
+        </div>
+      </div>`;
+
+      //Append the HTML to the #app
+      app.innerHTML = table;
+    }
+
+    // xhr.onload = function () {
+    //   let data = JSON.parse(xhr.response);
+    //   console.log(data);
+    // }
+  }
+
+  return {
+    load: function () {
+      alert('LOADED');
+      viewUsers();
     }
   }
-    return {
-      load: function(){
-        alert('LOADED');
-        viewUsers();
-      }
-    }
-  
-  })();
-  
-  usersApp.load();
+
+})();
+
+usersApp.load();
