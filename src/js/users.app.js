54b77ef125cc46ce76ea4663f9c378f6011b3438
pmatsuda <pat.matsuda@comcast.net>
2019-10-22 21:17:15 -0500

    Add delete of users

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -282,7 +282,9 @@ var usersApp = (function () {
           <div class="text-center">
             <br>
             <a class="btn btn-lg btn-danger text-white">
+            <a onclick="usersApp.deleteUser('${data.user._id}');" class="btn btn-lg btn-danger text-white">
               Yes delete ${data.user.username}
+            </a>
             </a>
           </div>
   
@@ -291,6 +293,29 @@ var usersApp = (function () {
   
       app.innerHTML = card;
     }
+  }
+
+  function deleteUser(id){
+
+    let uri = `${window.location.origin}/api/users/${id}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('DELETE', uri);
+  
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+  
+    xhr.send();
+  
+    xhr.onload = function(){
+      let data = JSON.parse(xhr.response);
+      if(data.success === true){
+        window.location.hash = '#';
+      }else{
+        alert('Unknown error, the user could not be deleted');
+      }  
+    }  
   }
 
   return {
@@ -321,6 +346,10 @@ var usersApp = (function () {
           viewUsers();
           break;
       }
+    },
+
+    deleteUser: function(id){
+      deleteUser(id);
     }
   }
 })();
