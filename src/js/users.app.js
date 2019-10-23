6c5c514420364fb5ead093047822aeeeaaac0242
pmatsuda <pat.matsuda@comcast.net>
2019-10-22 20:40:10 -0500

    Add get data for selected user

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -146,6 +146,25 @@ var usersApp = (function () {
       }
     }
 
+    function editUser(id){
+
+      let uri = `${window.location.origin}/api/users/${id}`;
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', uri);
+    
+      xhr.setRequestHeader(
+        'Content-Type',
+        'application/json; charset=UTF-8'
+      );
+    
+      xhr.send();
+    
+      xhr.onload = function(){
+        let data = JSON.parse(xhr.response);
+        console.log(data);
+      }
+    }
+
     function postRequest(formId, url){
       let form = document.getElementById(formId);
       form.addEventListener('submit', function(e){
@@ -191,11 +210,11 @@ var usersApp = (function () {
           break;
 
         case '#view':
-          console.log('VIEW');
+          viewUser(hashArray[1]);
           break;
 
         case '#edit':
-          console.log('EDIT');
+          editUser(hashArray[1]);
           break;
 
         case '#delete':
